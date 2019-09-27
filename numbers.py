349ea6aa9e6b0a2c30cef8fe1664a1abffd4f0da
Pradeep Kumar <pradeep.kumar@nmg-lp-9.newmediaguruorg.com>
2019-09-27 15:15:33 +0530

    numbers and operators

---
+++ numbers.py
@@ -0,0 +1,6 @@
+intgerNumber = 253
+floatingNumber = 1.025
+
+print(type(intgerNumber))
+
+print(type(floatingNumber))
---
+++ operators.py
@@ -0,0 +1,27 @@
+#Operators in python
+
+#Assignment Operator
+totalMarks = 45
+print(totalMarks)
+#Arthmatic Operator
+#5 types
+#+-*/%
+print(2+2)
+print(10 -2)
+print(4*4)
+print(12//4)
+print(13%4)
+
+
+#Logical Operator
+# AND OR NOT
+print(5 > 2 and 2)
+print(0 < -1 or 4)
+print(not 0)
+#Relational Operator
+# < > == >= <= != 
+test = 4
+if test == 5:
+    print('ok')
+else:
+    print('not')
